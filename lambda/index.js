30c5e8d3d95fb3911fdbb2b29e12f4296f152784
refluster <refluster@gmail.com>
2018-04-04 07:34:07 +0900

    lambda fn update for weightscale

--- lambda/index.js
+++ lambda/index.js
@@ -1,22 +1,9 @@
 const needle = require('needle')
 
 exports.handler = (event, context, callback) => {
-	console.log('handler', event);
-
-	if (event.data !== undefined) {
-		console.log('============ data');
-		let d = event.data;
-		/*
-		let url = 'https://api.push7.jp/api/v1/' + d.app_no + '/send';
-		let body = {
-			title: d.title,
-			icon: 'https://www.google.com/i.png',
-			body: d.body,
-			url: d.url,
-			apikey: d.api_key,
-		};
-
-		needle.post(url, body, { json: true }, (error, response) => {
+	function post(body) {
+		console.log('body: %j', body);
+		needle.post(process.env.HTTP_URL, { body }, { json: true }, (error, response) => {
 			if (error || response.body.result !== 'SUCCESS') {
 				console.log('Unable to emit event', response.body.toString())
 				return callback()
@@ -24,7 +11,18 @@ exports.handler = (event, context, callback) => {
 			console.log('post done by needle', response.body.toString())
 			callback(null, { statusCode: 200 })
 		})
-		insertDB(d);
-		*/
+	}
+
+	console.log('event: %j', event);
+
+	if (event.weightscale !== undefined) {
+		let d = event.weightscale;
+		let body = [{
+			topic: 'event',
+			action: 'emit',
+			eventName: d.eventName,
+			data: d.data
+		}];
+		post(body);
 	}
 }
--- lambda/test.sh
+++ lambda/test.sh
@@ -1,1 +1,1 @@
-curl -X POST https://ljusw3bf2h.execute-api.ap-northeast-1.amazonaws.com/v0/bath/weightscale -H 'content-type: application/json' -d '{"body":"this is body"}'
+curl -X POST https://ljusw3bf2h.execute-api.ap-northeast-1.amazonaws.com/v0/bath/weightscale -H 'content-type: application/json' -d '{"data": {"wind": {"power": 30} }, "eventName": "test_push"}'
