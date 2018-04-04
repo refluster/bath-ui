11ae15c3864a7fde7a7c46d17d3938557f219c92
refluster <refluster@gmail.com>
2018-04-04 22:12:34 +0900

    needle to post (not work)

--- site/src/app/controller.component.ts
+++ site/src/app/controller.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef } from '@angular/core';
 import { Headers, Http } from '@angular/http';
+import needle from 'needle';
 import { MsgService } from './service/msg.service';
 
 @Component({
@@ -14,6 +15,19 @@ export class ControllerComponent {
 	ngOnInit(): void {
 	}
 
-	change(): void {
+	push_weightcale(): void {
+		const url = 'https://ljusw3bf2h.execute-api.ap-northeast-1.amazonaws.com/v0/bath/weightscale';
+		const body = {data:
+					  {wind:
+					   {power: 30}
+					  },
+					  eventName: 'test_push'
+					 a};
+		console.log(needle);
+		needle.post(url, body, { json: true }, (error, response) => {
+			console.log('error: %j', error);
+			console.log('response: %j', response);
+			console.log('post done by needle %j', response.body);
+		});
 	}
 }
