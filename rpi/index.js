ea243472dcdc393552110021f302c841ad932a77
refluster <refluster@gmail.com>
2018-04-06 15:32:51 +0900

    deepstream-io server fixed to northeast-1

--- rpi/index.js
+++ rpi/index.js
@@ -1,6 +1,7 @@
 let deepstream = require('deepstream.io-client-js');
 
-var client = deepstream('13.56.190.2:6020');
+var client = deepstream('52.192.206.13:6020');
+//var client = deepstream('13.56.190.2:6020');
 client.login();
 client.event.subscribe('esdc/bath/test', data => {
     console.log('sub: data', data);
