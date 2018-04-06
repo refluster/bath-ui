80dda2715c9699a58a1cd11ae8d9a21405a0c23e
refluster <refluster@gmail.com>
2018-04-06 15:32:53 +0900

    deepstream-io server fixed to northeast-1

--- site/src/app/wall-display.component.ts
+++ site/src/app/wall-display.component.ts
@@ -11,9 +11,9 @@ export class WallDisplayComponent {
 	constructor(private msgService: MsgService) {}
 
 	ngOnInit(): void {
-		//var client = deepstream('52.192.206.13:6020');
+		let client = deepstream('52.192.206.13:6020');
 		// temporary use
-		let client = deepstream('13.56.190.2:6020');
+		//let client = deepstream('13.56.190.2:6020');
 		client.login();
 		client.event.subscribe('esdc/bath/test', d => {
 			console.log('deep steam io', d);
