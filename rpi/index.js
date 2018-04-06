9c7377b75ec63443f36c829c1cc5df577b3c86c6
refluster <refluster@gmail.com>
2018-04-06 15:33:39 +0900

    refactor

--- rpi/index.js
+++ rpi/index.js
@@ -1,7 +1,6 @@
 let deepstream = require('deepstream.io-client-js');
+let client = deepstream('52.192.206.13:6020');
 
-var client = deepstream('52.192.206.13:6020');
-//var client = deepstream('13.56.190.2:6020');
 client.login();
 client.event.subscribe('esdc/bath/test', data => {
     console.log('sub: data', data);
