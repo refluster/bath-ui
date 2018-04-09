0eb8450c4c42a73de8635e6ba3e4ebc646dd03db
refluster <refluster@gmail.com>
2018-04-09 06:20:27 +0000

    gpio control from deepstream-io

--- rpi/index.js
+++ rpi/index.js
@@ -1,5 +1,8 @@
 let deepstream = require('deepstream.io-client-js');
 let client = deepstream('52.192.206.13:6020');
+let Gpio = require('onoff').Gpio;
+
+let led = new Gpio(4, 'out');
 
 client.login();
 client.event.subscribe('esdc/bath/test', data => {
@@ -7,8 +10,10 @@ client.event.subscribe('esdc/bath/test', data => {
 		data.voicectrl.light !== undefined) {
 		if (data.voicectrl.light == true) {
 			console.log('light on');
+			led.writeSync(1);
 		} else {
 			console.log('light off');
+			led.writeSync(0);
 		}
 	}
-})
+});
